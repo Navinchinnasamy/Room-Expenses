14b0cd22e7e19bf1a3a3691548210b680b2b6018
Navin <navinchinnasamy@gmail.com>
2017-08-07 09:40:14 +0530

    Fixed redirect after save the details.

--- config/app.php
+++ config/app.php
@@ -177,6 +177,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+		// Form Provider - Custom added
+		'Collective\Html\HtmlServiceProvider',
     ],
 
     /*
@@ -226,6 +228,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+		'Form' => 'Collective\Html\FormFacade',
+		'Html' => 'Collective\Html\HtmlFacade',
     ],
 
 ];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,48 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- CSRF Token -->
+		<meta name="csrf-token" content="{{ csrf_token() }}">
+		<title>Login</title>
+		  <link rel="stylesheet" href="{{ asset('css/login/style.css') }}">
+		  <script>
+			window.Laravel = {!! json_encode([
+				'csrfToken' => csrf_token(),
+			]) !!};
+		</script>
+	</head>
+	<body>
+		<form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
+		  {{ csrf_field() }}
+		  <div class="group">
+			<input type="email" name="email" id="email" value="{{ old('email') }}" /><span class="highlight"></span><span class="bar"></span>
+			@if ($errors->has('email'))
+				<span class="help-block">
+					<strong>{{ $errors->first('email') }}</strong>
+				</span>
+			@endif
+			<label>Email</label>
+		  </div>
+		  <div class="group">
+			<input type="password" name="password" id="password" /><span class="highlight"></span><span class="bar"></span>
+			@if ($errors->has('password'))
+				<span class="help-block">
+					<strong>{{ $errors->first('password') }}</strong>
+				</span>
+			@endif
+			<label>Password</label>
+		  </div>
+		  <button type="submit" class="button buttonBlue">Login
+			<div class="ripples buttonRipples"><span class="ripplesCircle"></span></div>
+		  </button>
+		</form>
+		<footer><a href="http://www.navin.comze.com/mybot" target="_blank"><img src="https://www.polymer-project.org/images/logos/p-logo.svg"></a>
+		  <p>You Gotta Love <a href="http://www.navin.comze.com/mybot" target="_blank">Navin</a></p>
+		</footer>
+		<script src="{{ asset('plugins/jquery.min.js') }}"></script>
+		<script src="{{ asset('js/login/index.js') }}"></script>
+	</body>
+</html>
